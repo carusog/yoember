7a66ef9097afc68e440e038bf6e96502487898ee
Giuseppe Caruso <giuseppe@bonsai-studio.net>
2019-05-01 10:16:40 +0200

    add saveInvitation action

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,8 +7,22 @@ import Controller from '@ember/controller';
 export default Controller.extend({
 
   emailAddress: '',
+  responseMessage: '',
 
   isValid: match('emailAddress', /^.+@.+\..+$/),
-  isDisabled: not('isValid')
+  isDisabled: not('isValid'),
+
+  actions: {
+
+    saveInvitation() {
+      alert(`Saving of the following email address is in progress: ${this.emailAddress}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.emailAddress}`);
+      this.set('emailAddress', '');
+      setTimeout(() => {
+        this.set('responseMessage', '');
+      }, 3000);
+    }
+
+  }
 
 });
